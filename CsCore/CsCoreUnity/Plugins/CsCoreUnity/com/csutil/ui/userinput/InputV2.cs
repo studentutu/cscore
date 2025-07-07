173241da67c4b3008a7c8c9d975206916abcf303
S <code@csutil.com>
2025-07-07 19:54:35 +0200

    Implemented mapper to the new Untiy Input System

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/userinput/InputV2.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/userinput/InputV2.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
 
 namespace com.csutil {
 
@@ -10,15 +13,19 @@ namespace com.csutil {
 
         [Obsolete("Instead use instance version by first calling GetInputSystem()")]
         public static bool GetKeyUp(KeyCode keyCode) { return GetInputSystem().GetKeyUp(keyCode); }
-        
+
         [Obsolete("Instead use instance version by first calling GetInputSystem()")]
         public static bool GetKey(KeyCode keyCode) { return GetInputSystem().GetKey(keyCode); }
 
         [Obsolete("Instead use instance version by first calling GetInputSystem()")]
         public static bool GetMouseButton(int button) { return GetInputSystem().GetMouseButton(button); }
 
+#if ENABLE_INPUT_SYSTEM
+        private static readonly Func<IUnityInputSystem> DefaultInputSystemFactory = () => new NewUnityInputSystem();
+#else
         private static readonly Func<IUnityInputSystem> DefaultInputSystemFactory = () => new LegacyUnityInputManager();
-        
+#endif
+
         public static IUnityInputSystem GetInputSystem() { return IoC.inject.GetOrAddSingleton(null, DefaultInputSystemFactory); }
 
         public static bool GetMouseButtonDown(int button) { return GetInputSystem().GetMouseButtonDown(button); }
@@ -40,7 +47,57 @@ namespace com.csutil {
         public int touchCount => Input.touchCount;
         public bool GetMouseButton(int button) { return Input.GetMouseButton(button); }
         public bool GetMouseButtonDown(int button) { return Input.GetMouseButtonDown(button); }
-        
+
     }
+
+#if ENABLE_INPUT_SYSTEM
+    public class NewUnityInputSystem : IUnityInputSystem {
+
+        public bool GetKeyUp(KeyCode keyCode) {
+            if (Keyboard.current == null) { return false; }
+            // This is a simplified mapping. A full mapping would require a large switch or dictionary.
+            return Keyboard.current[Map(keyCode)].wasReleasedThisFrame;
+        }
+
+        public bool GetKey(KeyCode keyCode) {
+            if (Keyboard.current == null) { return false; }
+            // This is a simplified mapping. A full mapping would require a large switch or dictionary.
+            return Keyboard.current[Map(keyCode)].isPressed;
+        }
+
+        public int touchCount => Touchscreen.current?.touches.Count ?? 0;
+
+        public bool GetMouseButton(int button) {
+            if (Mouse.current == null) { return false; }
+            switch (button) {
+                case 0: return Mouse.current.leftButton.isPressed;
+                case 1: return Mouse.current.rightButton.isPressed;
+                case 2: return Mouse.current.middleButton.isPressed;
+                default: return false;
+            }
+        }
+
+        public bool GetMouseButtonDown(int button) {
+            if (Mouse.current == null) { return false; }
+            switch (button) {
+                case 0: return Mouse.current.leftButton.wasPressedThisFrame;
+                case 1: return Mouse.current.rightButton.wasPressedThisFrame;
+                case 2: return Mouse.current.middleButton.wasPressedThisFrame;
+                default: return false;
+            }
+        }
+
+        // A simple mapping from KeyCode to the new Input System's Key enum
+        private Key Map(KeyCode key) {
+            try {
+                return (Key)Enum.Parse(typeof(Key), key.ToString());
+            } catch (ArgumentException) {
+                Log.e($"KeyCode {key} does not have a direct mapping in the new Input System's Key enum.");
+                return Key.None;
+            }
+        }
+
+    }
+#endif
 
 }
